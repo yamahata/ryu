b8cff46c4aaab9359cccec67f1d888ceab884107
Isaku Yamahata <yamahata@valinux.co.jp>
2013-06-06 15:28:02 +0900

    app/quantum_adapter: move specific options to quantum adapter module
    move registration of quantum adapter into quantum adapter module.
    Good for modularization.
    
    Signed-off-by: Isaku Yamahata <yamahata@valinux.co.jp>

--- ryu/flags.py
+++ ryu/flags.py
@@ -28,6 +28,7 @@ def _register_cli_options(module_name):
         pass
 
 
+_register_cli_options('ryu.app.quantum_adapter')
 _register_cli_options('ryu.app.wsgi')
 _register_cli_options('ryu.log')
 _register_cli_options('ryu.topology.switches')
@@ -36,34 +37,6 @@ _register_cli_options('ryu.topology.switches')
 
 # global options
 CONF = cfg.CONF
-
-
-CONF.register_cli_opts([
-    # app/quantum_adapter
-    cfg.StrOpt('neutron-url', default='http://localhost:9696',
-               help='URL for connecting to neutron',
-               deprecated_name='quantum-url'),
-    cfg.IntOpt('neutron-url-timeout', default=30,
-               help='timeout value for connecting to neutron in seconds',
-               deprecated_name='quantum-url-timeout'),
-    cfg.StrOpt('neutron-admin-username', default='neutron',
-               help='username for connecting to neutron in admin context',
-               deprecated_name='quantum-admin-username'),
-    cfg.StrOpt('neutron-admin-password', default='service_password',
-               help='password for connecting to neutron in admin context',
-               deprecated_name='quantum-admin-password'),
-    cfg.StrOpt('neutron-admin-tenant-name', default='service',
-               help='tenant name for connecting to neutron in admin context',
-               deprecated_name='quantum-admin-tenant-name'),
-    cfg.StrOpt('neutron-admin-auth-url', default='http://localhost:5000/v2.0',
-               help='auth url for connecting to neutron in admin context',
-               deprecated_name='quantum-admin-auth-url'),
-    cfg.StrOpt('neutron-auth-strategy', default='keystone',
-               help='auth strategy for connecting to neutron in admin'
-               'context',
-               deprecated_name='quantum-auth-strategy'),
-    cfg.StrOpt('neutron-controller-addr', default=None,
-               help='openflow method:address:port to set controller of'
-               'ovs bridge',
-               deprecated_name='quantum-controller-addr')
-])
+# nothing yet
+# CONF.register_cli_options(...)
+# CONF.register_options(...)
